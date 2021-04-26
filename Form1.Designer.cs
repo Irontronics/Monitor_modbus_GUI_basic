ba61713393fdd4ae5707ee93fa269988a6781c86
irontronics <perezbautista.miguel@gmail.com>
2021-04-26 13:17:49 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace testing1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MONITOR MODBUS TCP/IP";
+            this.label1.UseMnemonic = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Puerto";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(70, 427);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Velocidad (RPM): ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label5.Location = new System.Drawing.Point(39, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 22);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Voltaje potencia: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label6.Location = new System.Drawing.Point(36, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 22);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Driver EN/DIS:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label7.Location = new System.Drawing.Point(37, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(181, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Paro de emergencia: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label8.Location = new System.Drawing.Point(39, 296);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(170, 22);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Temperatura ( ºC ): ";
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label10.Location = new System.Drawing.Point(211, 427);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(171, 20);
+            this.label10.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Cerrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(227, 92);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 36);
+            this.label9.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(229, 152);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 20);
+            this.label11.TabIndex = 12;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.Red;
+            this.label12.Location = new System.Drawing.Point(229, 193);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 20);
+            this.label12.TabIndex = 13;
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(227, 234);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(138, 36);
+            this.label13.TabIndex = 14;
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(227, 294);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(138, 36);
+            this.label14.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 451);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Monitor Modbus app";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace testing1
+{
+    public partial class Form1 : Form
+    {
+
+        string dato;      //para los datos 
+        sbyte index0fZ, index0fY, index0fX, index0fW, index0fV;
+        String dataMod1, dataMod2, dataMod3, dataMod4, dataMod5; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] puertos = SerialPort.GetPortNames(); //obtener puertos disponibles en pc
+
+            foreach (string mostrar in puertos) //mostrar en combobox 
+            {
+                comboBox1.Items.Add(mostrar);
+
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.Close(); //revisar que este cerrado el puerto
+            serialPort1.Dispose();
+            string puertoseleccionado = comboBox1.Text;
+            serialPort1.PortName = puertoseleccionado;
+            serialPort1.Open();
+            CheckForIllegalCrossThreadCalls = false;
+            if (serialPort1.IsOpen)
+            {
+
+                label10.Text = "Esta abierto";
+            }
+            else {
+                return; 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            serialPort1.Dispose();
+            Close();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e) // cuando se recibe un dato de arduino a Visual studio ! 
+        {
+            dato = serialPort1.ReadLine(); //INICO DE COMUNICACIÓN 
+            this.BeginInvoke(new EventHandler(ProcessData));
+
+
+
+
+        }
+
+        private void ProcessData(object sender, EventArgs e)
+        {
+            try
+            {
+
+                index0fZ = Convert.ToSByte(dato.IndexOf("Z"));
+                index0fY = Convert.ToSByte(dato.IndexOf("Y"));
+                index0fX = Convert.ToSByte(dato.IndexOf("X"));
+                index0fW = Convert.ToSByte(dato.IndexOf("W"));
+                index0fV = Convert.ToSByte(dato.IndexOf("V"));
+
+
+                dataMod1 = dato.Substring(0, index0fZ);
+                dataMod2 = dato.Substring(index0fZ + 1, (index0fY - index0fZ) - 1);
+                dataMod3 = dato.Substring(index0fY + 1, (index0fX - index0fY) - 1 );
+                dataMod4 = dato.Substring(index0fX + 1, (index0fW - index0fX) - 1);
+                dataMod5 = dato.Substring(index0fW + 1, (index0fV - index0fW) - 1);
+
+                double numero1 = Convert.ToDouble(dataMod1); //convertir dato a double 
+                double numero2 = Math.Round(((numero1 * 245735) / 4294967295), 2);
+                if (numero2 > 10) { label9.Text = numero2.ToString(); } else { label9.Text = "0";  }
+                
+                if (Convert.ToString(dataMod2) == "1") { label11.BackColor = Color.Green; } else if (Convert.ToString(dataMod2) == "0") { label11.BackColor = Color.Red; }
+                if (Convert.ToString(dataMod3) == "1") { label12.BackColor = Color.Green; } else if (Convert.ToString(dataMod3) == "0") { label12.BackColor = Color.Red; }
+
+                double numero3 = Convert.ToDouble(dataMod4); //convertir dato a double 
+                double numero4 = numero3 / 1000;
+                numero4 = Math.Round(numero4, 2);
+                label13.Text = numero4.ToString();
+
+                label14.Text = Convert.ToString(dataMod5); 
+
+
+
+            }
+            catch(Exception error) {
+
+                MessageBox.Show(error.Message); 
+            
+            }
+
+
+        }
+    }
+}
